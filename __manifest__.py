986d93dab9c79dc102b6553b0becb19339365262
Charlie-abcd <c.emani@allbconsulting.com>
2025-08-01 12:01:31 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Product Barcode on Documents",
-    "version": "2.0",
+    "version": "3.0",
     "author": "ABCD",
     "depends": ["account", "product", "sale", "purchase"],
     "data": [
@@ -8,9 +8,11 @@
         "views/report_invoice_barcode.xml",
         "views/sale_order_form.xml",
         "views/purchase_order_form.xml",
+        "views/stock_picking_form.xml",
         "views/report_saleorder_barcode.xml",
         "views/report_purchaseorder_barcode.xml",
         "views/report_purchasequotation_barcode.xml",
+        "views/report_stock_picking_barcode.xml",
     ],
     "installable": True,
     "application": False,
